5b9760882f512f3bbdd71729a2a58bdb56cb60e9
JonnyWalker <John.Witulski@uni-duesseldorf.de>
2018-08-05 14:42:19 +0200

    remove argparse

--- venv/pysnes/tool/pySNES.py
+++ venv/pysnes/tool/pySNES.py
@@ -1,15 +1,12 @@
 from cartrige import ROMHeader
 from helper import open_as_byte_array
 from disassembler import Disassembler
-import argparse
+import sys
 
-parser = argparse.ArgumentParser(description='SNES Emulator in RPython')
-parser.add_argument('rom', help='SNES ROM File')
-args = parser.parse_args()
-
-ba = open_as_byte_array(args.rom)
+ba = open_as_byte_array(sys.argv[1])
+print(len(ba))
 #print_hex_dump(ba)
 header = ROMHeader(ba)
 header.dump()
 d = Disassembler()
-d.print_assembler(ba, 0, 32)
+d.print_assembler(ba, 0, 129)
