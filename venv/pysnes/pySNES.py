061074bdbf1b85ef4db85bfb1b5fe07d8eee3511
JonnyWalker <John.Witulski@uni-duesseldorf.de>
2018-08-13 12:11:01 +0200

    Add PySNES args (start and end of ROM dump)

--- venv/pysnes/pySNES.py
+++ venv/pysnes/pySNES.py
@@ -3,10 +3,19 @@ from helper import open_as_byte_array
 from disassembler import Disassembler
 import sys
 
+if len(sys.argv) <= 1:
+    print("usage: python PySNES.py ROM_PATH [start] [end]")
+    exit(0)
+
 ba = open_as_byte_array(sys.argv[1])
-print(len(ba))
 #print_hex_dump(ba)[0:32]
 header = ROMHeader(ba)
 header.dump()
 d = Disassembler()
-d.print_assembler(ba, 0, 38)
+start = 0
+end = len(ba)-2
+if len(sys.argv) > 2:
+    start = int(sys.argv[2])
+if len(sys.argv) > 3:
+    end = int(sys.argv[3])
+d.print_assembler(ba, start, end)
