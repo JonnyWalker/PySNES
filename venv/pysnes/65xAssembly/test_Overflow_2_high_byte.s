94ea2434f2c1d7b40f85964980e5d814092d71f2
JonnyWalker <John.Witulski@uni-duesseldorf.de>
2018-08-16 20:59:36 +0200

    Fix Assembly example

--- venv/pysnes/65xAssembly/test_Overflow_2_high_byte.s
+++ venv/pysnes/65xAssembly/test_Overflow_2_high_byte.s
@@ -1,6 +1,6 @@
 ; Displays green WHEN FF+1 is zero (8Bit mode)
 ;
-; ca65 test_Overflow_2_high_byte.s
+; ca65 test_Overflow_2_high_byte.s 
 ; ld65 -C lorom128.cfg -o test_Overflow_2_high_byte.smc test_Overflow_2_high_byte.o
 
 .define ROM_NAME "GREEN"
@@ -19,11 +19,11 @@ reset:
 ; RELEVANT PART
     lda #$FF
     inc
-    SEP #$20  ; A 16-bit
+    REP #$21  ; A 16-bit, clear zero flag
+    .a16
     CMP #$0000
     beq green ; jump to green if A is zero. 
-    ; This means INC in 8Bit Mode does not write the high byte in 16 Bit Mode!
-    ; In 8 Bit Mode INC of x00FF is NOT x0100!
+    ; This means FF+1 in 8Bit Mode does not write the high byte in 16 Bit Mode!
 ; END PART
 
     ; Set background color to red ($001F)
