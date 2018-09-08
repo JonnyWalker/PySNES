af66cf137f13be8ab9b8d649d0d8284642c560f1
lulan102 <lulan102@uni-duesseldorf.de>
2018-09-08 19:18:00 +0200

    Added Flag tests to TCD, TCS, TDC and TSC tests

--- venv/pysnes/test/test_cpu_tcd.py
+++ venv/pysnes/test/test_cpu_tcd.py
@@ -26,6 +26,32 @@ def test_tcd():
     assert cpu.DP == 0x1234
     assert cpu.P == 0b0000000  # no flag
 
+def test_tcd_zero():
+    mem = MemoryMock()
+    cpu = CPU65816(mem)
+    cpu.P = 0b00000000  # 16 Bit mode
+    cpu.e = 0
+    cpu.A = 0x0000
+
+    cpu.fetch_decode_execute([0x5B])
+
+    assert cpu.cycles == 2
+    assert cpu.DP == 0x0000
+    assert cpu.P == 0b0000010  # zero flag
+
+def test_tcd_negative():
+    mem = MemoryMock()
+    cpu = CPU65816(mem)
+    cpu.P = 0b00000000  # 16 Bit mode
+    cpu.e = 0
+    cpu.A = 0xF234
+
+    cpu.fetch_decode_execute([0x5B])
+
+    assert cpu.cycles == 2
+    assert cpu.DP == 0xF234
+    assert cpu.P == 0b10000000  # negative flag
+
 
 def test_tcs_16():
     mem = MemoryMock()
@@ -40,20 +66,75 @@ def test_tcs_16():
     assert cpu.SP == 0x1234
     assert cpu.P == 0b0000000  # no flag
 
+def test_tcs_16_zero():
+    mem = MemoryMock()
+    cpu = CPU65816(mem)
+    cpu.P = 0b00000000  # 16 Bit mode
+    cpu.e = 0
+    cpu.A = 0x0000
+
+    cpu.fetch_decode_execute([0x1B])
+
+    assert cpu.cycles == 2
+    assert cpu.SP == 0x0000
+    assert cpu.P == 0b0000010  # zero flag
+
+def test_tcs_16_negative():
+    mem = MemoryMock()
+    cpu = CPU65816(mem)
+    cpu.P = 0b00000000  # 16 Bit mode
+    cpu.e = 0
+    cpu.A = 0xF234
+
+    cpu.fetch_decode_execute([0x1B])
+
+    assert cpu.cycles == 2
+    assert cpu.SP == 0xF234
+    assert cpu.P == 0b10000000  # negative flag
+
 
 def test_tcs_8():
     mem = MemoryMock()
     cpu = CPU65816(mem)
-    cpu.P = 0b00000000  # 16 Bit mode
+    cpu.P = 0b00100000  # 8 Bit mode
     cpu.e = 1
-    cpu.A = 0x1234
+    cpu.A = 0x34
     cpu.SP = 0x3412
 
     cpu.fetch_decode_execute([0x1B])
 
     assert cpu.cycles == 2
     assert cpu.SP == 0x3434
-    assert cpu.P == 0b0000000  # no flag
+    assert cpu.P == 0b00100000  # no flag
+
+def test_tcs_8_zero():
+    mem = MemoryMock()
+    cpu = CPU65816(mem)
+    cpu.P = 0b00100000  # 8 Bit mode
+    cpu.e = 1
+    cpu.A = 0x00
+    cpu.SP = 0x3412
+
+    cpu.fetch_decode_execute([0x1B])
+
+    assert cpu.cycles == 2
+    assert cpu.SP == 0x3400
+    assert cpu.P == 0b00100010  # zero flag
+
+def test_tcs_8_negative():
+    mem = MemoryMock()
+    cpu = CPU65816(mem)
+    cpu.P = 0b00100000  # 8 Bit mode
+    cpu.e = 1
+    cpu.A = 0x80
+    cpu.SP = 0x3412
+
+    cpu.fetch_decode_execute([0x1B])
+
+    assert cpu.cycles == 2
+    assert cpu.SP == 0x3480
+    assert cpu.P == 0b10100000  # negative flag
+
 
 def test_tdc():
     mem = MemoryMock()
@@ -68,6 +149,32 @@ def test_tdc():
     assert cpu.A == 0x1234
     assert cpu.P == 0b0000000  # no flag
 
+def test_tdc_zero():
+    mem = MemoryMock()
+    cpu = CPU65816(mem)
+    cpu.P = 0b00000000  # 16 Bit mode
+    cpu.e = 0
+    cpu.DP = 0x0000
+
+    cpu.fetch_decode_execute([0x7B])
+
+    assert cpu.cycles == 2
+    assert cpu.A == 0x0000
+    assert cpu.P == 0b0000010  # zero flag
+
+def test_tdc_negative():
+    mem = MemoryMock()
+    cpu = CPU65816(mem)
+    cpu.P = 0b00000000  # 16 Bit mode
+    cpu.e = 0
+    cpu.DP = 0xF234
+
+    cpu.fetch_decode_execute([0x7B])
+
+    assert cpu.cycles == 2
+    assert cpu.A == 0xF234
+    assert cpu.P == 0b10000000  # negative flag
+
 def test_tsc():
     mem = MemoryMock()
     cpu = CPU65816(mem)
@@ -80,3 +187,29 @@ def test_tsc():
     assert cpu.cycles == 2
     assert cpu.A == 0x1234
     assert cpu.P == 0b0000000  # no flag
+
+def test_tsc_zero():
+    mem = MemoryMock()
+    cpu = CPU65816(mem)
+    cpu.P = 0b00000000  # 16 Bit mode
+    cpu.e = 0
+    cpu.SP = 0x0000
+
+    cpu.fetch_decode_execute([0x3B])
+
+    assert cpu.cycles == 2
+    assert cpu.A == 0x0000
+    assert cpu.P == 0b0000010  # zero flag
+
+def test_tsc_negative():
+    mem = MemoryMock()
+    cpu = CPU65816(mem)
+    cpu.P = 0b00000000  # 16 Bit mode
+    cpu.e = 0
+    cpu.SP = 0xF234
+
+    cpu.fetch_decode_execute([0x3B])
+
+    assert cpu.cycles == 2
+    assert cpu.A == 0xF234
+    assert cpu.P == 0b10000000  # negative flag
