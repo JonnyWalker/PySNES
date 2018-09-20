347a11bfdeddeb821413653580a5d8e948254fc9
L0RCH <lukas.tf.lang@gmail.com>
2018-09-20 18:15:39 +0200

    Added Tests for TSB and TRB opcodes

---
+++ venv/pysnes/test/test_cpu_trb_tsb.py
@@ -0,0 +1,166 @@
+from pysnes.cpu import CPU65816
+
+# .../PySNES/venv/$ py.test pysnes/test/
+
+
+class MemoryMock(object):
+    def __init__(self):
+        self.ram = {}
+
+    def read(self, address):
+        return self.ram[address]
+
+    def write(self, address, value):
+        self.ram[address] = value
+
+
+def test_TRB_DP_16bit():
+    mem = MemoryMock()
+    cpu = CPU65816(mem)
+    cpu.P = 0b00000010  # 16 Bit mode
+    cpu.e = 0
+    cpu.DP = 0x1200
+    cpu.A = 0x0FAB
+
+    mem.write(0x001234, 0xAB)
+    mem.write(0x001235, 0x0F)
+
+    cpu.fetch_decode_execute([0x14, 0x34])
+
+    assert mem.read(0x001234) == 0x00
+    assert mem.read(0x001235) == 0x00
+    assert cpu.cycles == 7
+    assert cpu.A == 0x0FAB
+    assert cpu.P == 0b00000000
+
+
+def test_TRB_DP_8bit():
+    mem = MemoryMock()
+    cpu = CPU65816(mem)
+    cpu.P = 0b00100000  # 8 Bit mode
+    cpu.e = 0
+    cpu.DP = 0x1200
+    cpu.A = 0x9A
+
+    mem.write(0x001234, 0x65)
+
+    cpu.fetch_decode_execute([0x14, 0x34])
+
+    assert mem.read(0x001234) == 0x65
+    assert cpu.cycles == 5
+    assert cpu.A == 0x9A
+    assert cpu.P == 0b00100010
+
+
+def test_TRB_absolute_16bit():
+    mem = MemoryMock()
+    cpu = CPU65816(mem)
+    cpu.P = 0b00000010 # 16 Bit mode
+    cpu.e = 0
+    cpu.DBR = 0x12
+    cpu.A = 0xFFFF
+
+    mem.write(0x123456, 0xFF)
+    mem.write(0x123457, 0x0F)
+
+    cpu.fetch_decode_execute([0x1C, 0x56, 0x34])
+
+    assert mem.read(0x123456) == 0x00
+    assert mem.read(0x123457) == 0x00
+    assert cpu.cycles == 8
+    assert cpu.A == 0xFFFF
+    assert cpu.P == 0b00000000
+
+
+def test_TRB_absolute_8bit():
+    mem = MemoryMock()
+    cpu = CPU65816(mem)
+    cpu.P = 0b00100000 # 8 Bit mode
+    cpu.e = 0
+    cpu.DBR = 0x12
+    cpu.A = 0x66
+
+    mem.write(0x123456, 0x77)
+
+    cpu.fetch_decode_execute([0x1C, 0x56, 0x34])
+
+    assert mem.read(0x123456) == 0x11
+    assert cpu.cycles == 6
+    assert cpu.A == 0x66
+    assert cpu.P == 0b00100000
+
+
+def test_TSB_DP_16bit():
+    mem = MemoryMock()
+    cpu = CPU65816(mem)
+    cpu.P = 0b00000000  # 16 Bit mode
+    cpu.e = 0
+    cpu.DP = 0x1200
+    cpu.A = 0x6666
+
+    mem.write(0x001234, 0x99)
+    mem.write(0x001235, 0x99)
+
+    cpu.fetch_decode_execute([0x04, 0x34])
+
+    assert mem.read(0x001234) == 0xFF
+    assert mem.read(0x001235) == 0xFF
+    assert cpu.cycles == 7
+    assert cpu.A == 0x6666
+    assert cpu.P == 0b00000010
+
+
+def test_TSB_DP_8bit():
+    mem = MemoryMock()
+    cpu = CPU65816(mem)
+    cpu.P = 0b00100000  # 8 Bit mode
+    cpu.e = 0
+    cpu.DP = 0x1200
+    cpu.A = 0x9A
+
+    mem.write(0x001234, 0x65)
+
+    cpu.fetch_decode_execute([0x04, 0x34])
+
+    assert mem.read(0x001234) == 0xFF
+    assert cpu.cycles == 5
+    assert cpu.A == 0x9A
+    assert cpu.P == 0b00100010
+
+
+def test_TSB_absolute_16bit():
+    mem = MemoryMock()
+    cpu = CPU65816(mem)
+    cpu.P = 0b00000010 # 16 Bit mode
+    cpu.e = 0
+    cpu.DBR = 0x12
+    cpu.A = 0xFFFF
+
+    mem.write(0x123456, 0xFF)
+    mem.write(0x123457, 0x0F)
+
+    cpu.fetch_decode_execute([0x0C, 0x56, 0x34])
+
+    assert mem.read(0x123456) == 0xFF
+    assert mem.read(0x123457) == 0xFF
+    assert cpu.cycles == 8
+    assert cpu.A == 0xFFFF
+    assert cpu.P == 0b00000000
+
+
+def test_TSB_absolute_8bit():
+    mem = MemoryMock()
+    cpu = CPU65816(mem)
+    cpu.P = 0b00100000 # 8 Bit mode
+    cpu.e = 0
+    cpu.DBR = 0x12
+    cpu.A = 0x43
+
+    mem.write(0x12ABCD, 0x9C)
+
+    cpu.fetch_decode_execute([0x0C, 0xCD, 0xAB])
+
+    assert mem.read(0x12ABCD) == 0xDF
+    assert cpu.cycles == 6
+    assert cpu.A == 0x43
+    assert cpu.P == 0b00100010
